8cd8c32d8d54eef1a2d13af6339b54fe7f82af1e
mhung889 <daomanh889@gmail.com>
2025-12-14 19:03:20 +0700

    fix route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,26 +5,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './pages/student/Login';
 import ForgotPassword from './pages/student/ForgotPassword';
 import ResetPassword from './pages/student/ResetPassword';
+
 import StudentDashboard from './pages/student/StudentDashboard';
 import StudentProfile from './pages/student/StudentProfile';
 import StudentNotifications from './pages/student/StudentNotifications';
-import MentorNotificationsPage from './pages/mentor/MentorNotificationsPage'
-import AdminLayout from "./pages/admin/AdminLayout"
-import AdminOverviewPage from "./pages/admin/AdminOverviewPage"
-import AdminLabsPage from "./pages/admin/AdminLab/AdminLabsPage"
-import AdminMentorsPage from "./pages/admin/AdminMentorsPage"
-import AdminStudentsPage from "./pages/admin/AdminStudentsPage"
-import MentorLayout from "./pages/mentor/MentorLayout"
-import MentorDashboard from "./pages/mentor/MentorDashboard"
-import MentorTasksPage from "./pages/mentor/MentorTasksPage"
-import MentorStudentsPage from "./pages/mentor/MentorStudentsPage"
-
-
-import LeaveRequestStudent from "./pages/student/LeaveRequestStudent";
+import LeaveRequestStudent from './pages/student/leave-request/LeaveRequestStudent';
 import FaceRegisterPage from './pages/student/FaceRegisterPage';
 import FaceAttendancePage from './pages/student/FaceAttendancePage';
-
-import LeaveRequestStudent from './pages/student/leave-request/LeaveRequestStudent';
 
 import AdminLayout from './pages/admin/AdminLayout';
 import AdminOverviewPage from './pages/admin/AdminOverviewPage';
@@ -47,54 +34,48 @@ function App() {
   return (
     <>
       <Toaster duration={1500} />
-
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<HomePage />} />
-
-          {/* student */}
+          {/* public */}
           <Route path='/login' element={<Login />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/reset-password' element={<ResetPassword />} />
-          <Route path='/student' element={<StudentDashboard />} />
-          <Route path='/student/profile' element={<StudentProfile />} />
-          <Route path='/student/notifications' element={<StudentNotifications />} />
-          <Route path='/student/leave' element={<LeaveRequestStudent />} />
-          <Route path="/student/register-face" element={<FaceRegisterPage />} />
-          <Route path="/student/attendance" element={<FaceAttendancePage />} />
-
-          {/* public */}
-          <Route path="/login" element={<Login />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/reset-password" element={<ResetPassword />} />
-          <Route path="/403" element={<Forbidden />} />
+          <Route path='/403' element={<Forbidden />} />
 
           {/* protected */}
           <Route element={<RequireAuth />}>
             {/* student */}
             <Route element={<RequireRole allowedRoles={['student']} />}>
-              <Route path="/student" element={<StudentDashboard />} />
-              <Route path="/student/profile" element={<StudentProfile />} />
+              <Route path='/student' element={<StudentDashboard />} />
+              <Route path='/student/profile' element={<StudentProfile />} />
               <Route
-                path="/student/notifications"
+                path='/student/notifications'
                 element={<StudentNotifications />}
               />
-              <Route path="/student/leave" element={<LeaveRequestStudent />} />
+              <Route path='/student/leave' element={<LeaveRequestStudent />} />
+              <Route
+                path='/student/register-face'
+                element={<FaceRegisterPage />}
+              />
+              <Route
+                path='/student/attendance'
+                element={<FaceAttendancePage />}
+              />
             </Route>
 
             {/* mentor */}
             <Route element={<RequireRole allowedRoles={['mentor']} />}>
-              <Route path="/mentor" element={<MentorLayout />}>
+              <Route path='/mentor' element={<MentorLayout />}>
                 <Route index element={<MentorDashboard />} />
-                <Route path="dashboard" element={<MentorDashboard />} />
-                <Route path="tasks" element={<MentorTasksPage />} />
-                <Route path="students" element={<MentorStudentsPage />} />
+                <Route path='dashboard' element={<MentorDashboard />} />
+                <Route path='tasks' element={<MentorTasksPage />} />
+                <Route path='students' element={<MentorStudentsPage />} />
                 <Route
-                  path="leave-requests"
+                  path='leave-requests'
                   element={<MentorLeaveRequestsPage />}
                 />
                 <Route
-                  path="notifications"
+                  path='notifications'
                   element={<MentorNotificationsPage />}
                 />
               </Route>
@@ -102,20 +83,20 @@ function App() {
 
             {/* admin */}
             <Route element={<RequireRole allowedRoles={['admin']} />}>
-              <Route path="/admin" element={<AdminLayout />}>
+              <Route path='/admin' element={<AdminLayout />}>
                 <Route index element={<AdminOverviewPage />} />
-                <Route path="overview" element={<AdminOverviewPage />} />
-                <Route path="labs" element={<AdminLabsPage />} />
-                <Route path="mentors" element={<AdminMentorsPage />} />
-                <Route path="students" element={<AdminStudentsPage />} />
-                <Route path="majors" element={<AdminMajorsPage />} />
+                <Route path='overview' element={<AdminOverviewPage />} />
+                <Route path='labs' element={<AdminLabsPage />} />
+                <Route path='mentors' element={<AdminMentorsPage />} />
+                <Route path='students' element={<AdminStudentsPage />} />
+                <Route path='majors' element={<AdminMajorsPage />} />
               </Route>
             </Route>
           </Route>
 
           {/* default */}
-          <Route path="/" element={<Navigate to="/login" replace />} />
-          <Route path="*" element={<Navigate to="/login" replace />} />
+          <Route path='/' element={<Navigate to='/login' replace />} />
+          <Route path='*' element={<Navigate to='/login' replace />} />
         </Routes>
       </BrowserRouter>
     </>
